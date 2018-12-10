fd66de3b2a14ee739e0e160d5d4ec22899df316d
Arun <arun@mobileLIVEs-MacBook-Pro.local>
2018-12-10 10:55:06 -0500

    Initial Tour of Heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
